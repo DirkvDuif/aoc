06a73b69685b480cbeea786f1372169e3fc682b1
Dirk van Duivenbooden <d.vanduivenbooden@ruitenburg.nu>
2023-12-14 09:27:45 +0100

    working day 3 part 1

--- 2023/day3/day3.py
+++ 2023/day3/day3.py
@@ -1,35 +1,30 @@
-with open("./day3/input.txt", "r", encoding="utf-8") as f:
+with open("./2023/day3/input.txt", "r", encoding="utf-8") as f:
     lines = f.read().split("\n")
 
 total = 0
 for i in range(len(lines)):
     digit = ""
     symbol_found = False
-    first = None
-    last = None
     for j in range(len(lines[i])):
         if lines[i][j].isdigit():
             # WE HAVE FOUND A DIGIT
             digit += lines[i][j]
-            last = (i, j)
-            if first is None:
-                first = (i, j)
+            # FIND SYMBOL
+            for y in range(i - 1, i + 2):
+                if not y < 0 and y < len(lines) :
+                    for x in  range(j - 1, j + 2):
+                        if not x < 0 and x < len(lines[i]):
+                            if not lines[y][x] == "." and not lines[y][x].isdigit():
+                                symbol = lines[y][x]
+                                symbol_found = True
         else:
-            if not first == None:
-                for y in range(first[0] - 1, last[0] + 1):
-                    if y < 0 or y > len(lines) - 1:
-                        continue
-                    for x in  range(first[1] - 1, last[1] + 1):
-                        if x < 0 or x > len(lines[i]):
-                            continue
-                        if not lines[y][x] == "." and not lines[y][x].isdigit():
-                            symbol_found = True
-                if symbol_found == True and digit != "":
-                    total += int(digit)
-                    digit = ""
-                    symbol_found = False
-                    first, last = None, None
-
-            digit = ""
+            if symbol_found == True and digit != "":
+                total += int(digit)
             symbol_found = False
+            digit = ""
+    else:
+        if symbol_found == True and digit != "":
+            total += int(digit)
+        symbol_found = False
+        digit = ""
 print(total)
